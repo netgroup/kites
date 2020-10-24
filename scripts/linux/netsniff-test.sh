388b3f16a2353bb21c907f73485a0413845be872
Francesco Lombardo <franclombardo@gmail.com>
2020-10-24 09:52:30 +0200

    add check valid dir

--- scripts/linux/netsniff-test.sh
+++ scripts/linux/netsniff-test.sh
@@ -15,14 +15,20 @@ TEMP_IP_SRC=$(sed -e "s/\r//g" <<<$IP_SRC)
 TEMP_IP_DEST=$(sed -e "s/\r//g" <<<$IP_DEST)
 NEW_IP_SRC=$(sed -e "s/\"//g" <<<$TEMP_IP_SRC)
 NEW_IP_DEST=$(sed -e "s/\"//g" <<<$TEMP_IP_DEST)
-cd /pod-shared/"$FOLDER_POD"
+
+if [ -d "/pod-shared/$FOLDER_POD" ] 
+then
+    cd /pod-shared/$FOLDER_POD
+else
+    mkdir -p /pod-shared/"$FOLDER_POD" && cd /pod-shared/$FOLDER_POD
+fi
 echo -e "FROM VM_SRC: $POD_HOSTNAME_1 TO VM_DEST: $POD_HOSTNAME_2 \n" >$PCAP.txt
 echo -e "FROM POD_SRC: $POD_NAME_1 TO POD_DEST: $POD_NAME_2 \n" >>$PCAP.txt
 echo -e "FROM IP_SRC: $IP_SRC TO IP_DEST: $IP_DEST - TIMESTAMP: $TIMESTAMP - ID_EXP: $ID_EXP \n" >>$PCAP.txt
-echo -e "$PCAP - PPS: ${PPS} - BYTE: ${BYTE}\n" >>$PCAP.txt
+echo -e "$PCAP - PPS: ${PPS} - BYTE: ${BYTE}\n" >>"$PCAP".txt
 #timeout -s SIGINT 13 netsniff-ng --in eth0 --ring-size 64MiB --silent --no-sock-mem --filter "src host 10.244.96.1 and dst host 10.244.128.1"  >> $PCAP.txt
 timeout -s SIGINT 15 netsniff-ng --in eth0 --ring-size 64MiB --silent --no-sock-mem --filter "src host $NEW_IP_SRC and dst host $NEW_IP_DEST" >>$PCAP.txt
-echo -e "\n\n" >>$PCAP.txt
-cat $PCAP.txt >>NETSNIFF-${BYTE}byte-${PPS}pps.txt
-rm $PCAP.txt
+echo -e "\n\n" >>"$PCAP".txt
+cat "$PCAP".txt >>NETSNIFF-"${BYTE}"byte-"${PPS}"pps.txt
+rm "$PCAP".txt
 exit
--- scripts/linux/trafgen-test.sh
+++ scripts/linux/trafgen-test.sh
@@ -11,7 +11,12 @@ BYTE=$9
 PPS=${10}
 NUM=$((PPS * 10))
 
-cd /pod-shared/$FOLDER_POD
+if [ -d "/pod-shared/$FOLDER_POD" ] 
+then
+    cd /pod-shared/$FOLDER_POD
+else
+    mkdir -p /pod-shared/"$FOLDER_POD" && cd /pod-shared/$FOLDER_POD
+fi
 echo -e "FROM VM_SRC: $POD_HOSTNAME_1 TO VM_DEST: $POD_HOSTNAME_2 \n" >$CONFIG_FILE_UDP_TRAFFIC.txt
 echo -e "FROM POD_SRC: $POD_NAME_1 TO POD_DEST: $POD_NAME_2 \n" >>$CONFIG_FILE_UDP_TRAFFIC.txt
 echo -e "FROM IP_SRC: $IP_SRC TO IP_DEST: $IP_DEST \n" >>$CONFIG_FILE_UDP_TRAFFIC.txt
@@ -20,6 +25,7 @@ echo -e "$CONFIG_FILE_UDP_TRAFFIC - PPS: ${PPS} \n" >>$CONFIG_FILE_UDP_TRAFFIC.t
 timeout -s SIGINT 10 trafgen --in $CONFIG_FILE_UDP_TRAFFIC --out eth0 --no-sock-mem --qdisc-path --rand --rate ${PPS}pps >>$CONFIG_FILE_UDP_TRAFFIC.txt
 #trafgen --in $CONFIG_FILE_UDP_TRAFFIC --out eth0 --no-sock-mem --qdisc-path --rand --rate ${PPS}pps --num ${NUM} >> $CONFIG_FILE_UDP_TRAFFIC.txt
 echo -e "\n\n" >>$CONFIG_FILE_UDP_TRAFFIC.txt
+
 cat $CONFIG_FILE_UDP_TRAFFIC.txt >>TRAFGEN-${BYTE}byte-${PPS}pps.txt
 rm $CONFIG_FILE_UDP_TRAFFIC.txt
 exit
