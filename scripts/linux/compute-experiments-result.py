d0422c2528db9eac3345dd55c22daec56e80ef12
Carla <carla.santangelo95@gmail.com>
2021-01-23 00:06:21 +0100

    wip diffnodes stats

--- scripts/linux/compute-experiments-result.py
+++ scripts/linux/compute-experiments-result.py
@@ -7,13 +7,17 @@ import sys
 # cni=sys.argv[1]
 # EXP_N=sys.argv[2]
 # bytes=sys.argv[3]
+# N=sys.argv[4]
 # kites_home="/vagrant/ext/kites/"
 
 kites_home="c:/Users/carla/Documents/kubernetes-playground/ext/kites/"
 cni="weavenet"
 EXP_N=5
 byte=100
-
+pps_min=13000
+pps_max=20000
+pps_inc=500
+N=3
 rep= EXP_N + 1
 print(cni)
 
@@ -23,8 +27,9 @@ total= pd.read_csv(kites_home + "tests/"+ cni +"/exp-1/cpu-usage-" + cni + "-UDP
 # remove spurious rows and take the values
 indexNames = total[ total['PPS'] == 'pps' ].index
 total= total.drop(indexNames)
+info_columns = total.iloc[:, [0,3]]
 total = total.iloc[:, 4:10]
-
+ 
 for exp_n in range(2, rep, 1):
     #define exp name
     id_exp="exp-"+str(exp_n)
@@ -40,4 +45,48 @@ for exp_n in range(2, rep, 1):
 
 total = total/5
 print(total)
+print(info_columns)
+total = pd.concat([info_columns, total], axis=1)
+total.columns = total.columns.str.replace(' ', '') 
+print(total)
+# df[0] = df[0].str.strip()
+total['TEST_TYPE'] = total['TEST_TYPE'].str.replace(' ', '') 
+print(total)
 
+
+
+rows = []
+for pps in range(pps_min, pps_max+pps_inc, pps_inc):
+    print(pps)
+    index_pps = total[ total['PPS'] == ''+str(pps)+'' ].index
+    print(index_pps)
+    pps_df= total.loc[index_pps]
+    print(pps_df)
+    print(list(pps_df.columns.values))
+    n=1
+    cpu_tx_sum=0
+    cpu_rx_sum=0
+    for minion_i in range(1, N+1, 1):
+        for minion_j in range(1, N+1, 1):
+            if minion_i != minion_j:
+                string_name='k8s-minion-'+ str(minion_i) +'TOk8s-minion-'+ str(minion_j) +''
+                print(string_name)
+                row_conf = pps_df[ pps_df['TEST_TYPE'] == 'k8s-minion-'+ str(minion_i) +'TOk8s-minion-'+ str(minion_j) +'' ].index
+                col_tx = "cpu-from-minion-"+str(minion_i)
+                col_rx = "cpu-from-minion-"+str(minion_j)
+
+                rxtx= pps_df.loc[row_conf, "rx/tx"].item()
+                txedtx= pps_df.loc[row_conf, "txed/totx"].item()
+
+                cpu_tx = pps_df.loc[row_conf, col_tx]
+                cpu_tx_sum = ((cpu_tx_sum + cpu_tx)).item()
+                cpu_rx = pps_df.loc[row_conf, col_rx]
+                cpu_rx_sum = ((cpu_rx_sum + cpu_rx)).item()
+                n=n+1
+
+    cpu_rx_avg = cpu_rx_sum/n
+    cpu_tx_avg = cpu_tx_sum/n
+    rows.append([pps, rxtx, txedtx, cpu_tx_avg, cpu_rx_avg])            
+
+diffnode = pd.DataFrame(rows, columns=["PPS", "rx/tx", "txed/totx", "cpu_tx", "cpu_rx"])
+print(diffnode)
