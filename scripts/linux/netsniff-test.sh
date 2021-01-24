33794893ba0c82e66f83a8abbb78910e0a11921b
Francesco Lombardo <franclombardo@gmail.com>
2021-01-24 20:58:29 +0100

    fix filter netsniff-ng

--- scripts/linux/netsniff-test.sh
+++ scripts/linux/netsniff-test.sh
@@ -21,7 +21,7 @@ echo -e "FROM POD_SRC: $POD_NAME_1 TO POD_DEST: $POD_NAME_2 \n" >> $PCAP.txt
 echo -e "FROM IP_SRC: $IP_SRC TO IP_DEST: $IP_DEST - TIMESTAMP: $TIMESTAMP - ID_EXP: $ID_EXP \n" >> $PCAP.txt
 echo -e "$PCAP - PPS: ${PPS} - BYTE: ${BYTE}\n" >> $PCAP.txt
 #timeout -s SIGINT 13 netsniff-ng --in eth0 --ring-size 64MiB --silent --no-sock-mem --filter "src host 10.244.96.1 and dst host 10.244.128.1"  >> $PCAP.txt
-timeout -s SIGINT 15 netsniff-ng --in eth0 --ring-size 64MiB --silent --no-sock-mem --filter "src host $NEW_IP_SRC and dst host $NEW_IP_DEST"  >> $PCAP.txt
+timeout -s SIGINT 15 netsniff-ng --in eth0 --ring-size 64MiB --silent --no-sock-mem --filter "src host $NEW_IP_SRC and dst host $NEW_IP_DEST and inbound"  >> $PCAP.txt
 echo -e "\n\n" >> $PCAP.txt
 cat $PCAP.txt >> NETSNIFF-${BYTE}byte-${PPS}pps.txt
 rm $PCAP.txt
